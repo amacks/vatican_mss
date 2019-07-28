9158a943f366cea722a05fcae33b24fa842b817b
Aaron Macks <aaronm@wiglaf,org>
2019-07-27 23:41:02 -0400

    adding MS count footer

--- weekly_report.pl
+++ weekly_report.pl
@@ -173,6 +173,7 @@ sub format_mss_list{
 				'ms_base_url' => $ms_base_url,
 				'header_data' => $header_data,
 				'url_prefix' => $url_prefix,
+				'ms_count' => $#{$mss_list}+1,
 			);
 		my $output;
 		my $tt = Template->new({
