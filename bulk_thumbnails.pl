00a6087713ae947c12960ad5452c45faea71882e
Aaron Macks <aaronm@baal.wiglaf.org>
2019-09-26 15:54:17 -0400

    fixing a bunch of bugs in the bulk downloader

--- bulk_thumbnails.pl
+++ bulk_thumbnails.pl
@@ -1,21 +1,47 @@
+#!/usr/bin/perl -w 
 
+use strict;
+use POSIX;
+use POSIX qw(strftime);
+## for all the HTTP work
+use IO::Socket::SSL;
+## there's nothing to "use" but we need it installed for SSL to verify
+use Mozilla::CA;
+use LWP::UserAgent;
+use LWP::Simple;
+use Getopt::Long;
+
+use HTML::TreeBuilder::XPath;
+use Data::Dumper;
+use Encode;
+use utf8;
+
+## for the storage engine
+use DBD::mysql;
+use DBI qw(:sql_types);
+
+use File::Basename;
+use lib dirname($0) . "/lib/";
+use Vatican::Config;
+use Vatican::DB;
 my $filepath = undef; ## if defined, the root path where to output the file
 GetOptions(
 		'filepath=s' => \$filepath);
 
-if (defined($data->{'filepath'})){
+if (defined($filepath)){
 	my $config = new Vatican::Config();
 	my $ms_table = $config->ms_table();
 	## connect to a DB
 	my $vatican_db = new Vatican::DB();
 	my $dbh=$vatican_db->get_insert_dbh();
 ## now prepare a handle for the statement
-	my $report_stmt = " select shelfmark year(date_added) as year, thumbnail_url from manuscripts where thumbnail_url like 'http%'' and high_quality=1";
-	my $sth = $dbh->prepare($report_stmt) or die "cannot prepare report statement: ". $dbh->errstr();
+	my $report_stmt = " select shelfmark, year(date_added) as year, thumbnail_url from manuscripts where thumbnail_url like 'http%' and high_quality=1 limit 10";
+	my $sth = $dbh->prepare($report_stmt) or die "cannot prepare thumbnail statement: ". $dbh->errstr();
 	## now do the query
 	$sth->execute() or die "cannot run report: " . $sth->errstr();
 	while (my $row = $sth->fetchrow_hashref()){
-		my $local_filepath = $data->{'filepath'} . "/" . $row->{'year'} . '/thumbnails';
+		warn Dumper($row);
+		my $local_filepath = $filepath . "/" . $row->{'year'} . '/thumbnails';
 		my $local_filename =  $row->{'shelfmark'} . ".jpg";
 		my $http_response = getstore($row->{'thumbnail_url'}, $local_filepath . '/' . $local_filename);
 		if (!is_error($http_response)){
@@ -24,12 +50,11 @@ if (defined($data->{'filepath'})){
 				warn "Some sort of error setting the local thumbnail";
 			}
 		} else {
-			warn "Some sort of error in downloading the image for " . $shelfmark;
+			warn "Some sort of error in downloading the image for " . $row->{'shelfmark'};
 			warn $http_response;
 		}
 	}
 	$sth->finish();
 	$dbh->disconnect();
-	return $manuscripts;
 
 }
