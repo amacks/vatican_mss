54400d6933aaf6f659749058de4bd174a67e8ad7
Aaron Macks <aaronm@wiglaf.org>
2020-05-07 14:00:37 -0400

    better table name now that we are greek too

--- sql/pal_lat_table.sql
+++ sql/pal_lat_table.sql
@@ -4,9 +4,9 @@ the Virtual Palatine Library at the University of Heidelberg */
 
 SET NAMES utf8mb4;
 
-DROP TABLE IF EXISTS `pal_lat_sources`;
+DROP TABLE IF EXISTS `pal_lat_gr_sources`;
 
-CREATE TABLE `pal_lat_sources` (
+CREATE TABLE `pal_lat_gr_sources` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `shelfmark` varchar(32) NOT NULL,
   `url` varchar(256) DEFAULT NULL,
