579b3a8da296112d227811de91eac42c1315135f
Aaron Macks <aaronm@wiglaf.org>
2020-06-04 16:50:04 -0400

    put the right year values in the bookends

--- regenerate_all.sh
+++ regenerate_all.sh
@@ -7,7 +7,7 @@ CURRENT_YEAR=`date +%Y`
 CURRENT_WEEK=$((`date +%W` +1))
 
 for week in `seq ${START_WEEK} 53`; do
-	./weekly_report.pl --week=${week} --year=${year} --filepath=/apps/apache/htdocs/
+	./weekly_report.pl --week=${week} --year=${START_YEAR} --filepath=/apps/apache/htdocs/
 done
 for year in `seq $((${START_YEAR}+1)) $((${CURRENT_YEAR} - 1))`; do
 	for week in `seq 1 53`; do
@@ -15,7 +15,7 @@ for year in `seq $((${START_YEAR}+1)) $((${CURRENT_YEAR} - 1))`; do
 	done
 done
 for week in `seq 1 ${CURRENT_WEEK}`; do
-	./weekly_report.pl --week=${week} --year=${year} --filepath=/apps/apache/htdocs/
+	./weekly_report.pl --week=${week} --year=${CURRENT_YEAR} --filepath=/apps/apache/htdocs/
 done
 
 ./generate_index.pl --filepath=${FILEPATH}vatican/
