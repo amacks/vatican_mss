45c3a47f64d83cabc4c5e223ec7e365f29248dde
Aaron Macks <aaronm@wiglaf.org>
2020-06-15 11:03:58 -0400

    moving adhoc report templates to their own file

--- adhoc_reports.pl
+++ adhoc_reports.pl
@@ -24,36 +24,18 @@ use DBI qw(:sql_types);
 ## for formatting the output
 use Template;
 
-## timestamp formatting from 
-##https://stackoverflow.com/questions/2149532/how-can-i-format-a-timestamp-in-perl
-sub get_time 
-{
-    my $format = $_[0] || '%Y%m%d %I:%M:%S %p'; #default format: 20160801 10:48:03 AM
-    return strftime($format, localtime);
-}
-
-sub get_week_and_year(){
-	my $offset_date = DateTime->now();
-	my ($week_year, $week_number) = $offset_date->week;
-	return (
-		$week_year,
-		$week_number,
-	);
-}
 ## constants
-my $today_timestamp = get_time('%Y_%m_%d');
 my $base_url="https://digi.vatlib.it/mss";
 my $ms_base_url = "https://digi.vatlib.it/view/MSS_";
 my @collections=("Autogr.Paolo.VI","Barb.gr","Barb.lat","Barb.or","Bonc","Borg.Carte.naut","Borg.ar","Borg.arm","Borg.cin","Borg.copt","Borg.ebr","Borg.eg","Borg.et","Borg.gr","Borg.ill","Borg.ind","Borg.isl","Borg.lat","Borg.mess","Borg.pers","Borg.sir","Borg.tonch","Borg.turc","Borgh","Capp.Giulia","Capp.Sist","Capp.Sist.Diari","Cappon","Carte.Stefani","Carte.d'Abbadie","Cerulli.et","Cerulli.pers","Chig","Comb","De.Marinis","Ferr","Legat","Neofiti","Ott.gr","Ott.lat","P.I.O","Pagès","Pal.gr","Pal.lat","Pap.Bodmer","Pap.Hanna","Pap.Vat.copt","Pap.Vat.gr","Pap.Vat.lat","Patetta","Raineri","Reg.gr","Reg.gr.Pio.II","Reg.lat","Ross","Ruoli","S.Maria.Magg","S.Maria.in.Via.Lata","Sbath","Sire","Urb.ebr","Urb.gr","Urb.lat","Vat.ar","Vat.arm","Vat.copt","Vat.ebr","Vat.estr.or","Vat.et","Vat.gr","Vat.iber","Vat.ind","Vat.lat","Vat.mus","Vat.pers","Vat.sam","Vat.sir","Vat.slav","Vat.turc");
 my $DEBUG=0;
-my $inital_load_end = '2018-01-21 21:06:15';
 
-my $header = "";
-my $footer = "";
 my $url_prefix="/vatican";
 
 ## for the database
-my $report_stmt = "select 
+my $results_stmt = "select shelfmark, title, author, incipit, notes, thumbnail_url
+from
+(select 
  ms1.shelfmark as shelfmark, 
  ms1.high_quality as high_quality, 
  ms1.date_added as date_added, 
@@ -63,17 +45,15 @@ my $report_stmt = "select
  ms1.incipit as incipit,
  ms1.notes as notes,
  ms1.thumbnail_url as thumbnail_url,
- ms1.date as date from 
+ ms1.date as date,
+ ms1.sort_shelfmark from 
 __MS_TABLE__ as ms1 left join __MS_TABLE__ as ms2
-on ms1.shelfmark=ms2.shelfmark AND ms1.id>ms2.id 
+on ms1.shelfmark=ms2.shelfmark AND ms1.id>ms2.id) as hq_lq
  where
-(year(ms1.date_added) = ?) AND
-(week(ms1.date_added,0)+1 = ?)
-ORDER by ms1.sort_shelfmark asc";
+__QUERY__
+ORDER by sort_shelfmark asc";
 
-my $header_stmt = "select header_text, image_filename, boundry_image_filename, previous_week_id, next_week_id from __NOTES_TABLE__ 
-where  year=? and week_number=?  ";
-my $links_stmt = "select year, week_number from __NOTES_TABLE__ where id=?  ";
+my $header_stmt = "select header_text, short_title, footer_text, query, filename from adhoc_reports where enabled=1";
 
 
 ### handle arguments to set the offset values and decide if we're output to console or note
@@ -94,21 +74,18 @@ if (defined($today)){
 }
 
 my $config = new Vatican::Config();
-warn "Processing Week: ". $week_number . " of year ". $year;
 
-sub get_mss_interval{
-	my $week_number = shift;
-	my $year = shift;
+sub get_mss_listing{
+	my $query = shift;
 	## get the DB configs
 	my $ms_table = $config->ms_table();
 	## connect to a DB
 	my $vatican_db = new Vatican::DB();
 	my $dbh=$vatican_db->get_generate_dbh();
 ## now prepare a handle for the statement
-	$report_stmt =~ s/__MS_TABLE__/$ms_table/g;
-	my $sth = $dbh->prepare($report_stmt) or die "cannot prepare report statement: ". $dbh->errstr();
-	$sth->bind_param(1,$year, SQL_INTEGER);
-	$sth->bind_param(2,$week_number, SQL_INTEGER);
+	$results_stmt =~ s/__MS_TABLE__/$ms_table/g;
+	$results_stmt =~ s/__QUERY__/$query/g;
+	my $sth = $dbh->prepare($results_stmt) or die "cannot prepare results statement: ". $dbh->errstr();
 	## now do the query
 	$sth->execute() or die "cannot run report: " . $sth->errstr();
 	my $manuscripts = [];
@@ -136,38 +113,22 @@ sub get_header_data{
 	my $dbh=$vatican_db->get_generate_dbh();
 ## now prepare a handle for the statement
 	$header_stmt =~ s/__NOTES_TABLE__/$notes_table/g;
-	$links_stmt =~ s/__NOTES_TABLE__/$notes_table/g;
 
 	my $sth = $dbh->prepare($header_stmt) or die "cannot prepare report statement: ". $dbh->errstr();
-	$sth->bind_param(1,$year, SQL_INTEGER);
-	$sth->bind_param(2,$week_number, SQL_INTEGER);
 	## now do the query
 	$sth->execute() or die "cannot run report: " . $sth->errstr();
-	my $header_data= $sth->fetchrow_hashref();
-	## now markdown it
 	my $m = Text::Markdown->new;
-	$header_data->{'header_text_html'} = $m->markdown($header_data->{'header_text'});
+	my $headers_data;
+	while (my $header_data = $sth->fetchrow_hashref()){
+		$header_data->{'header_text_html'} = $m->markdown($header_data->{'header_text'}) if (defined($header_data->{'header_text'}));
+		$header_data->{'footer_text_html'} = $m->markdown($header_data->{'footer_text'}) if (defined($header_data->{'footer_text'}));
+		push @$headers_data, $header_data;	
+	}
+	## now markdown it
+
 	$sth->finish();
-	## figure out the previous and next links
-	my $link_sth = $dbh->prepare($links_stmt) or die "cannot prepare links statement: ". $dbh->errstr();
-	## get the previous
-	if (defined($header_data->{'previous_week_id'})){
-		$link_sth->execute($header_data->{'previous_week_id'});
-		my $row = $link_sth->fetchrow_hashref();
-		if (defined($row)){
-			$header_data->{'previous_link'} = $config->get_filename("/", $row->{'year'}, $row->{'week_number'});
-		}
-	}
-	if (defined($header_data->{'next_week_id'})){
-		$link_sth->execute($header_data->{'next_week_id'});
-		my $row = $link_sth->fetchrow_hashref();
-		if (defined($row)){
-			$header_data->{'next_link'} = $config->get_filename("/", $row->{'year'}, $row->{'week_number'});
-		}
-	}
-	$link_sth->finish();
 	$dbh->disconnect();
-	return $header_data;
+	return $headers_data;
 }
 sub format_mss_list{
 	my ($mss_list, $header_data) = @_;
@@ -178,7 +139,6 @@ sub format_mss_list{
 		## setup the template system
 		my %data = (
 				'mss_list' => $mss_list,
-				'datestamp_parts' => { 'week' => $week_number, 'year' => $year},
 				'ms_base_url' => $ms_base_url,
 				'header_data' => $header_data,
 				'url_prefix' => $url_prefix,
@@ -190,22 +150,28 @@ sub format_mss_list{
 		    INTERPOLATE  => 1,
 		    ENCODING     => 'utf8',
 		}) || die "$Template::ERROR\n";
-		$tt->process("weekly_listing.tt",
+		$tt->process("adhoc_listing.tt",
 			\%data, \$output, {binmode => ':utf8'}
 			)|| die $tt->error(), "\n";
-    	return $header . $output .$footer;
+    	return $output;
 	}
 }
 
 ## now actually run things!
-my $formatted_html = format_mss_list(get_mss_interval($week_number, $year),get_header_data($week_number, $year));
+warn "Getting reports listing";
+my $reports_data = get_header_data();
 if (!defined($filepath)){
-	print $formatted_html;
+	print "Warning: cannot export without filepath";
 } else {
-	my $filename = $config->get_filename($filepath,$year,$week_number);
+	foreach my $report (@{${reports_data}}) {
+		my $filename = $filepath . $report->{'filename'} . ".html";
+		warn "\tbuilding report $filename";
 
-	warn "writing to $filename";
-	open(OUTPUT_FILE, ">:utf8", $filename) or die "Could not open file '$filename'. $!";
-	print OUTPUT_FILE $formatted_html;
-	close(OUTPUT_FILE);
+		my $formatted_html = format_mss_list(get_mss_listing($report->{'query'}), $report);
+
+		open(OUTPUT_FILE, ">:utf8", $filename) or die "Could not open file '$filename'. $!";
+		print OUTPUT_FILE $formatted_html;
+		close(OUTPUT_FILE);
+
+	}
 }
