5b312096957eb4776d4110d52b930df702dca02d
Aaron Macks <aaronm@wiglaf.org>
2020-10-13 11:03:44 -0400

    line wrap at the end of printing shelfmarks

--- download_vatican_listings.pl
+++ download_vatican_listings.pl
@@ -235,8 +235,8 @@ for my $collection (@collections){
 	}
 }
 print "Done with " . ($#{$shelfmarks_inserted}+1) . " inserted. \n";
-for my $shelfmark (@$shelfmarks_inserted){
-	print "\t" . $shelfmark;
-}
+print "\t";
+print join(@$shelfmarks_inserted, " ");
+print "\n";
 post_import_update();
 
