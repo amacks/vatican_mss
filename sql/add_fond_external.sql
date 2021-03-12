588756d03e556cd453a2855ab1218d73f554204c
Aaron Macks <aaronm@wiglaf.org>
2021-03-11 19:24:02 -0500

    adding sql statement to set fond values on pre-fonded manuscripts

--- sql/add_fond_external.sql
+++ sql/add_fond_external.sql
@@ -1,5 +1,6 @@
 /* These SQL statements will add a fond_code column to the manuscripts table and set the 
-fonds.code column as an external constraint.  They must be run after the fonds table has been created */
+   fonds.code column as an external constraint.  They must be run after the fonds table 
+   has been created */
 
 ALTER TABLE `manuscripts` ADD `fond_code` VARCHAR(64)  
 	CHARACTER SET utf8mb4  COLLATE utf8mb4_general_ci NULL  DEFAULT NULL  AFTER `sort_shelfmark`;
@@ -10,3 +11,11 @@ ALTER TABLE `manuscripts`
 	ADD CONSTRAINT `fond_code_ext` FOREIGN KEY (`fond_code`) 
 	REFERENCES `vatican_mss`.`fonds` (`code`) 
 	ON DELETE SET NULL ON UPDATE CASCADE;
+
+/* now that the 2 tables are setup with external constraints, populate manuscripts 
+   where needed */
+
+update manuscripts as m join fonds as f
+	on m.shelfmark like concat(f.code, "%")
+	set m.fond_code=f.code
+	where m.fond_code is null
