91d48417fa9f0ebf0763015f4c83b845b9d9d63c
Aaron Macks <aaronm@wiglaf.org>
2021-04-20 10:29:46 -0400

    creating a detail class and basic unit test for processing the detail pages

---
+++ lib/Vatican/Detail.pm
@@ -0,0 +1,143 @@
+#!/usr/bin/perl
+
+package Vatican::Detail;
+## package for downloading and parsing the detail page from a manuscript
+
+use strict;
+use Exporter;
+
+use Data::Dumper;
+use Encode;
+use utf8;
+use Moose;
+use LWP;
+use LWP::UserAgent;
+use HTML::TreeBuilder::XPath;
+use List::Compare;
+
+use File::Basename;
+use lib dirname($0) . "/lib/";
+use Vatican::Config;
+
+## Constants
+my @ignore_entries = ( '1)(Shelfmark Only)'); ## array of meaningless entries to ignore
+my $DEBUG = undef;
+
+## Moose class variables
+
+has 'shelfmark' => (
+	is => 'ro',
+	isa => 'Str'
+);
+
+has 'description_html' => (
+	is => 'rw',
+	isa => 'Maybe[Str]'
+);
+
+has 'bib_entries' => (
+	is => 'rw',
+	isa => 'Maybe[ArrayRef]');
+
+has 'detail_entries' => (
+	is => 'rw',
+	isa => 'Maybe[ArrayRef]');
+
+
+sub BUILD{
+	my $this = shift;
+	if (!defined($this->shelfmark())){
+		warn "Cannot create a detail without a shelfmark";
+		return undef
+	} else {
+		$this->get_catalogue_data();
+	}
+}
+## takes the complete HTML of a description page and parses it to count the number of description items and the number 
+## of bibiligraphy entries
+sub process_description_html($){
+	my $this = shift;
+	## sometimes there are just self links.  Those are ignorable.
+	my $tree = HTML::TreeBuilder::XPath->new_from_content( $this->description_html() );
+	## HTML is not well formed. Need to get a list of all details and then subtract the bibliography
+		## each entry looks like
+	## <div class="row-title"><a href="/mss/detail/192068"><div class="row-mss-title">
+	## <div class="order">5)</div><div class="title">Ghilardi, Massimiliano 
+	## «Non fuimus et fuimus». Gaetano Marini e le reliquie, In Gaetano Marini 
+	## (1742-1815) protagonista della cultura europea: scritti per il bicentenario 
+	## della morte: II, a cura di Marco Buonocore (Studi e testi, 493), 2015
+	##</div></div></a>
+	my @all_entries = $tree->findvalues('//div[ @class="row-title" ]');
+	## section for bibliography begins <div class="bibliographic_ref_label">
+	my @bib_entries = $tree->findvalues('//div[ @class="bibliographic_ref_label" ]/following::div[ @class="row-title" ]');
+	## now add the bibliography entries to the ignore list
+	@ignore_entries = (@ignore_entries, @bib_entries);
+	my $lc = List::Compare->new( {
+    	lists    => [\@all_entries, \@ignore_entries]
+    	});
+	my @detail_entries = $lc->get_unique();
+	## now set some values, if they exist
+	$this->bib_entries(\@bib_entries);
+	$this->detail_entries(\@detail_entries);
+	return 1;
+}
+
+## gets the detail page and processes it.  Needs three arguments inside a hash
+## shelfmark
+## vatican_db handle for updating the record
+## base of the detail url from the config 
+sub get_catalogue_data($){
+	my $this = shift;
+
+	my $config = new Vatican::Config();
+	my $detail_base_url = $config->detail_base_url();
+
+	my $description_html = get_url_content(
+		$detail_base_url . $this->shelfmark());
+	if (defined($description_html)){
+		warn "we got a description page" if ($DEBUG);
+		$this->description_html($description_html);
+		$this->process_description_html();
+	} else {
+		warn "no description in BAV";
+	}
+}
+
+## helper functions for getting counts, since it's so common
+sub get_bib_count($){
+	my $this = shift;
+	return 1+$#{$this->bib_entries()};
+}
+sub get_detail_count($){
+	my $this = shift;
+	return 1+$#{$this->detail_entries()};
+}
+
+
+## Static functions
+## just a simple get of a url
+sub get_url_content($){
+	my $url = shift;
+	my $html_content;
+
+	my $ua=new LWP::UserAgent;
+    $ua->timeout(35);
+    
+    my $request = new HTTP::Request('GET', $url); 
+    my $response = $ua->request($request); 
+    
+    if ($response->is_error){
+        warn "Unable to retrieve URL $url: ". $response->status_line;
+        return undef;
+    } else {
+        warn " Page retrieved" if ($DEBUG);
+        $html_content = $response->content;
+        #warn $html_content if ($DEBUG);
+    }
+    ## decode utf8
+    $html_content= decode('UTF-8', $html_content);
+    return $html_content;
+}
+
+
+1;
---
+++ t/006_Detail.t
@@ -0,0 +1,80 @@
+use strict;
+use warnings;
+use Test::More tests => 17;
+use JSON;
+use Data::Dumper;
+
+## to be tested
+use lib "./lib";
+
+BEGIN {
+ use_ok('Vatican::Config'); 
+ use_ok('Vatican::Detail');
+}
+
+## shelfmarks to test
+my $bav_shelfmark_only = 'Borg.arm.2'; ## entry with shelfmark only on page
+my $bav_bib_only = 'Barb.lat.4'; ## bibliography, but no description
+my $bav_big_bib = 'Vat.lat.8914'; ## large bibliography
+my $bav_one_description = 'Vat.lat.15136'; ## single line of description
+my $bav_multiple_both = 'Arch.Cap.S.Pietro.H.83'; ## both bib and desvriptions
+my $bav_missing = '' ; ## this might be fixed, finally
+
+## Test all the creators
+my $shelfmark_only_detail;
+isa_ok ( 
+        $shelfmark_only_detail = Vatican::Detail->new(shelfmark => $bav_shelfmark_only), 
+        "Vatican::Detail", "Create a Detail object for no details"
+        );
+my $bib_only_detail;
+isa_ok ( 
+        $bib_only_detail = Vatican::Detail->new(shelfmark => $bav_bib_only), 
+        "Vatican::Detail", "Create a Detail object only bibliography"
+        );
+my $big_bib_detail;
+isa_ok ( 
+        $big_bib_detail = Vatican::Detail->new(shelfmark => $bav_big_bib), 
+        "Vatican::Detail", "Create a Detail object bib bibliography"
+        );
+my $one_description_detail;
+isa_ok ( 
+        $one_description_detail = Vatican::Detail->new(shelfmark => $bav_one_description), 
+        "Vatican::Detail", "Create a Detail object one line description"
+        );
+my $multiple_both_detail;
+isa_ok ( 
+        $multiple_both_detail = Vatican::Detail->new(shelfmark => $bav_multiple_both), 
+        "Vatican::Detail", "Create a Detail object with everything"
+        );
+
+## test the values returned
+ok (
+	$shelfmark_only_detail->get_bib_count() == 0, "no Bibliography for ". $bav_shelfmark_only
+	);
+ok (
+	$shelfmark_only_detail->get_detail_count() == 0, "no Details for ". $bav_shelfmark_only
+	);
+ok (
+	$bib_only_detail->get_bib_count() == 5, "5 Bibliography entries for ". $bav_bib_only
+	);
+ok (
+	$bib_only_detail->get_detail_count() == 0, "no Details for ". $bav_bib_only
+	);
+ok (
+	$big_bib_detail->get_bib_count() == 18, "18 Bibliography entries for ". $bav_big_bib
+	);
+ok (
+	$big_bib_detail->get_detail_count() == 0, "no Details for ". $bav_big_bib
+	);
+ok (
+	$one_description_detail->get_bib_count() == 0, "No Bibliography entries for ". $bav_one_description
+	);
+ok (
+	$one_description_detail->get_detail_count() == 1, "1 Detail for ". $bav_one_description
+	);
+ok (
+	$multiple_both_detail->get_bib_count() == 2, "2 Bibliography entries for ". $bav_multiple_both
+	);
+ok (
+	$multiple_both_detail->get_detail_count() == 4, "4 Detail for ". $bav_multiple_both
+	);
