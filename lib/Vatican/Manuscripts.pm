02b62ebf68923a65b11f173361c1471241149e84
Aaron Macks <aaronm@wiglaf.org>
2021-08-18 14:19:23 -0400

    fixing the entry date calculations in the manuscript class and using function to get the url

--- lib/Vatican/Manuscripts.pm
+++ lib/Vatican/Manuscripts.pm
@@ -68,7 +68,7 @@ has 'mss_stmt' => (
 	default => "select shelfmark, 
 	title, author, incipit, notes, 
 	thumbnail_url, date_added, lq_date_added, 
-	high_quality, date, fond_code
+	high_quality, date, fond_code, week, year
 from
 (select 
  coalesce(ms1.shelfmark) as shelfmark, 
@@ -83,7 +83,9 @@ from
  max(ms1.date) as date,
  max(ms1.sort_shelfmark) as sort_shelfmark,
  max(ms1.ignore) as `ignore`,
- max(ms1.fond_code) as fond_code
+ max(ms1.fond_code) as fond_code,
+ year(coalesce(max(ms1.date_added), max(ms2.date_added))) as year,
+ week(coalesce(max(ms1.date_added), max(ms2.date_added)), 4) as week
  from
 __MS_TABLE__ as ms1 left join __MS_TABLE__ as ms2
 on ms1.shelfmark=ms2.shelfmark AND ms1.id>ms2.id
@@ -152,10 +154,11 @@ sub load_manuscripts($){
 	return $#{$this->mss_list()};
 }
 
-## process the notes for markdown
+## process the notes for markdown and url
 sub post_process_manuscripts($){
 	my $this = shift;
 	my $m = Text::Markdown->new;
+	my $config = new Vatican::Config();
 	my $field_count=0; ## the number of fields with markdown processed. 
 	##equals # of fields per MS with defined values * MS Count
 	for (my $i=0;$i<=$#{$this->mss_list}; $i++){
@@ -164,7 +167,9 @@ sub post_process_manuscripts($){
 				$this->mss_list()->[$i]->{$field . "_html"} = $m->markdown($this->mss_list()->[$i]->{$field});
 				$field_count++;
 	    	}
-	    } 
+	    }
+	    ## now generate the proper URL for the entry with this manuscript
+ 		$this->mss_list()->[$i]->{'entry_url'} = $config->get_filename('',$this->mss_list()->[$i]->{'year'} ,$this->mss_list()->[$i]->{'week'} );
 	}
 	return $field_count;
 }
