f3e5dbf1d02a38c34b208ad11ef58db5b658c0f7
Aaron Macks <5543396+amacks@users.noreply.github.com>
2026-08-05 18:22:28 -0400

    adding single page generation to the scripts

--- daily_generate.sh
+++ daily_generate.sh
@@ -5,6 +5,7 @@ FILEPATH=/apps/apache/htdocs/
 ./weekly_report.pl --today --filepath=${FILEPATH}
 ./generate_index.pl --filepath=${FILEPATH}
 ./generate_rss.pl --filepath=${FILEPATH} --mss-limit=100
+./generate_ms_pages.pl --filepath=${FILEPATH} --days=3
 
 ## Make sure to sync up CSS
 cp css/* ${FILEPATH}vatican/css/
--- regenerate_all.sh
+++ regenerate_all.sh
@@ -23,6 +23,7 @@ done
 ./adhoc_reports.pl --filepath=${FILEPATH}
 ./fond_pages.pl --filepath=${FILEPATH}
 ./generate_rss.pl --filepath=${FILEPATH} --mss-limit=100
+./generate_ms_pages.pl --filepath=${FILEPATH} --days=0
 
 ## Make sure to sync up CSS
 cp css/* ${FILEPATH}vatican/css/
